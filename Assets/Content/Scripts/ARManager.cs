58d14d171f16256bd4c47063a52f389576e27ae7
F286 <free@debreuil.com>
2018-12-17 13:31:33 +0900

    Setting up Board Scene

---
+++ Assets/Content/Scripts/ARManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARManager : MonoBehaviour {
+
+  public bool hasPlaced = false;
+
+  [Space]
+  public ContentPositioningBehaviour contentPositioningBehaviour;
+
+  public void Awake() {
+    VuforiaRuntime.Instance.InitVuforia();
+  }
+
+  public void OnInteractiveHitTest(HitTestResult result) {
+    // print("interactive: " + result.Position);
+    if (!hasPlaced) {
+      hasPlaced = true;
+      contentPositioningBehaviour.PositionContentAtPlaneAnchor(result);
+    }
+  }
+
+  public void OnAutomaticHitTest(HitTestResult result) {
+    // print("automatic: " + result.Position);
+    
+  }
+
+}
---
+++ Assets/Content/Scripts/Gesture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Gesture : MonoBehaviour, IPointerClickHandler {
+
+  public void OnPointerClick(PointerEventData eventData) {
+    print(eventData.pointerCurrentRaycast.worldPosition);
+
+    GetComponent<MeshRenderer>().material.color = Color.red;
+  }
+
+}
