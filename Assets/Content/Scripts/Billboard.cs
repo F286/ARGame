d9a345e17a4618a406e086e79a4474d48e217d34
F286 <free@debreuil.com>
2018-12-19 00:25:23 +0900

    block billboards

---
+++ Assets/Content/Scripts/Billboard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+  void LateUpdate() {
+    var d = Camera.main.transform.position - transform.position;
+    transform.eulerAngles = new Vector3(0, Mathf.Atan2(d.x, d.z) * Mathf.Rad2Deg, 0);
+  }
+}
---
+++ Assets/Content/Scripts/BlockVisual.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockVisual : MonoBehaviour {
+
+  int lastUpdatedTurn = -1;
+
+  public CardPosition position;
+
+  [Space]
+  public GameObject billboard;
+  public GameObject[] templates;
+
+  public void Awake() {
+    foreach (var item in templates) {
+      item.SetActive(false);
+    }
+    billboard.SetActive(false);
+  }
+
+  public void Update() {
+    if (CardData.instance.IsDirty(ref lastUpdatedTurn)) {
+
+      foreach (Transform item in billboard.transform) {
+        if (item.name == "[COPY]") {
+          Destroy(item.gameObject);
+        }
+      }
+
+      var setBillboardActive = false;
+
+      foreach (var item in CardData.instance.GetCards()) {
+        if (item.position == position) {
+          var copy = GameObject.Instantiate(templates[(int)item.type], billboard.transform);
+          copy.name = "[COPY]";
+          copy.SetActive(true);
+
+          setBillboardActive = true;
+        }
+      }
+
+      billboard.SetActive(setBillboardActive);
+    }
+  }
+
+}
